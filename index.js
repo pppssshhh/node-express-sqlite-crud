e02fbc5e0a855bdcc9725beb88e8662e6af50078
pppssshhh <111171978+pppssshhh@users.noreply.github.com>
2023-05-13 18:04:28 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const Comments = sequelize.define('Comments', {
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+});
+
+(async () => {
+  await Comments.sync();
+})();
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs');
+
+// Read
+app.get('/', async function (req, res) {
+  const comments = await Comments.findAll();
+  res.render('index', { comments: comments});
+});
+
+// Create
+app.post('/create', async function (req, res) {
+  const { content } = req.body
+  await Comments.create({ content: content });
+  res.redirect('/')
+});
+
+// Update
+app.post('/update/:id', async function (req, res) {
+  const { content } = req.body
+  const { id } = req.params
+  await Comments.update({ content: content }, {
+    where: {
+      id: id
+    }
+  });
+  res.redirect('/')
+});
+
+// Delete
+app.post('/delete/:id', async function (req, res) {
+  const { id } = req.params 
+  await Comments.destroy({
+    where: {
+      id: id
+    }
+  });
+  res.redirect('/')
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
